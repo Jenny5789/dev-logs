bde336348962023a82b8f68fa575a8bd26669ce2
EJae Choi <ejae2195@naver.com>
2026-05-29 22:45:49 +0900

    Delete student_manager directory

--- student_manager/static/main.js
+++
@@ -1,6 +0,0 @@
-// 알림 메시지 3초 후 자동 제거
-document.querySelectorAll(".alert").forEach(el => {
-  setTimeout(() => el.style.opacity = "0", 2800);
-  setTimeout(() => el.remove(), 3200);
-  el.style.transition = "opacity .4s";
-});
